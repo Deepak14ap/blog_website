31f0208c3c616c87ae0b3e51bf02b8b621fd89a1
Deepak14ap <68297956+Deepak14ap@users.noreply.github.com>
2021-08-20 09:57:30 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
 
-    <title>Welcome To PHPLoversBlog</title>
+    <title>Welcome To Tech Blog</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
